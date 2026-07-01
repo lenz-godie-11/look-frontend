cfd877d31fb205cb21f82ce359a97bddebba91e0
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-07-01 22:06:20 +0300

    commit

---
+++ look/src/components/signup.jsx
@@ -0,0 +1,18 @@
+function Signup() {
+  return (
+    <div>
+      <div>
+        <h1>create account</h1>
+        <div>
+          <label>email</label>
+          <input type="email" placeholder="email" />
+        </div>
+      </div>
+      <div>
+        <label>password</label>
+        <input type="password" placeholder="password" />
+      </div>
+    </div>
+  );
+}
+export default Signup;
