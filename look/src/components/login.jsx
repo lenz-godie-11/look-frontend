ba601187f5ff943cf6ea8add0a5f548b39c5a174
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-07-02 08:43:57 +0300

    commit

--- look/src/components/login.jsx
+++ look/src/components/login.jsx
@@ -1,19 +1,48 @@
+import useState from "react";
+
 function Login() {
+  const [credentials, setCredentials] = useState({
+    email: " ",
+    password: " ",
+  });
+
+  const [message,setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setCredentials({...credentials, [e.target.name]: e,target.value});
+  };
+
+  const handleSubmit = (e) =>{
+    e,preventDefault();
+    //perform login logic here and send to server 
+  }
+
   return (
     <div>
       <h1>LogIn</h1>
       <div>
         <label>email</label>
-        <input type="email" placeholder="email" />
+        <input
+          type="email"
+          placeholder="email"
+          value={credentials.email}
+          onChange={(e) =>
+            setCredentials({ ...credentials, email: e.target.value })
+          }
+        />
       </div>
       <div>
         <label>password</label>
-        <input type="password" placeholder="password" />
+        <input
+          type="password"
+          placeholder="password"
+          value={credentials.password}
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+        />
       </div>
-      <div>
-        <label>confirm</label>
-        <input type="password" placeholder="confirm" />
-      </div>
+
       <button type="submit">login</button>
     </div>
   );
