88a328df1768671da699b8b02bae42f8c3ed2da1
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-07-02 09:45:24 +0300

    commit

--- look/src/App.jsx
+++ look/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import LandingPage from "./components/home.jsx";
 import SignUp from "./components/signup.jsx";
 import LogIn from "./components/login.jsx";
+import ImageUpload from "./components/ImageUpload.jsx";
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState("landing");
@@ -13,6 +14,10 @@ function App() {
         <button onClick={() => setCurrentScreen("landing")}>Home Info</button>
         <button onClick={() => setCurrentScreen("login")}>Log In View</button>
         <button onClick={() => setCurrentScreen("signup")}>Sign Up View</button>
+
+        <button onClick={() => setCurrentScreen("upload")}>
+          Upload Picture
+        </button>
       </nav>
 
       <main>
@@ -23,6 +28,10 @@ function App() {
         {currentScreen === "signup" && <SignUp onNavigate={setCurrentScreen} />}
 
         {currentScreen === "login" && <LogIn onNavigate={setCurrentScreen} />}
+
+        {currentScreen === "upload" && (
+          <ImageUpload onNavigate={setCurrentScreen} />
+        )}
       </main>
     </>
   );
